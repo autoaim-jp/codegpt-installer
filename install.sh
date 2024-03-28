ab401bb0bd50deac5dd0ee18a79075df9cbdd8a3
autoaim_jp <git@autoaim.jp>
2024-03-29 03:07:14 +0900

    feat: improve CodeGPT installation script and setup
    - Create a new file `install.sh` with installation script for CodeGPT
    - Set up environment variables for CodeGPT installation path and update PATH
    
    Signed-off-by: autoaim_jp <git@autoaim.jp>

---
+++ install.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+sudo whoami
+
+sudo mkdir -p /opt/codegpt/bin/
+cd /opt/codegpt/bin/
+sudo chown ${USER}: -R ./
+
+wget https://github.com/appleboy/CodeGPT/releases/download/v0.10.0/CodeGPT-0.10.0-linux-amd64
+mv CodeGPT-0.10.0-linux-amd64 /opt/codegpt/bin/codegpt
+chmod +x /opt/codegpt/bin/codegpt
+
+cat<<"EOF" >> ~/.bashrc
+export CODEGPT_INSTALL="/opt/codegpt/"
+export PATH="$CODEGPT_INSTALL/bin:$PATH"
+EOF
+
+export CODEGPT_INSTALL="/opt/codegpt/"
+export PATH="$CODEGPT_INSTALL/bin:$PATH"
+
+codegpt version
+
